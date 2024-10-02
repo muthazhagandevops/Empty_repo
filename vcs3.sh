16a547d58e49a8cd310b7e9438193fa3a3d71491
muthazhagan <muthazhagan.sm@gmail.com>
2024-10-02 17:33:03 +0530

    3 sh files commit

---
+++ vcs3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+a = 10
+
+echo " value of a is $a "
+DATE = `date`
+echo date is $DATE
+
+USER = `who | wc -l`
+echo " Logged in user are $USER "
+
+UP = `uptime`
+echo " up time is $UP
+
+
